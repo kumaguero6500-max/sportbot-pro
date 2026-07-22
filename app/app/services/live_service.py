b62bbfd7da2822431cc42623acb9f3f4e6df52aa
kumaguero6500-max <kumaguero6500@gmail.com>
2026-07-22 15:05:54 +0300

    Update live_service.py

--- app/app/services/live_service.py
+++ app/app/services/live_service.py
@@ -1,1 +1,10 @@
+from app.api.api_football import APIFootball
 
+api = APIFootball()
+
+
+async def get_live_matches():
+
+    data = await api.get_live_matches()
+
+    return data.get("response", [])
